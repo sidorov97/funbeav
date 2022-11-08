8f1d1c88f72e5be95bf3af9aa3bc73347b6cb7bc
Ivan Sidorov <sidorov_i@rocketdata.io>
2022-11-08 13:14:49 +0300

    Upd8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Sidorov',                   # Type in your name
   author_email = 'sidorov_i@rocketdata.iocom',      # Type in your E-Mail
   url = 'https://github.com/sidorov97/funbeav',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sidorov97/funbeav/archive/refs/tags/v02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sidorov97/funbeav/archive/refs/tags/v03.tar.gz',    # I explain this later on
   keywords = ['SOME', 'SHIT', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
